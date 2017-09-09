1e00e85e0ee07f4adc0a0ce99fe50754e962480c
RazAlgethi <casse.clement@outlook.com>
2017-09-09 16:34:07 +0200

    Oops Forgot entrypoint.js while renaming React components.

--- src/entrypoint.jsx
+++ src/entrypoint.jsx
@@ -6,8 +6,8 @@ import Resume from './pages/resume/Resume';
 const app = document.getElementById('app');
 
 ReactDOM.render(
-  <div>
-    <Resume />
-  </div>,
-  app,
+    <div>
+        <Resume />
+    </div>,
+    app,
 );
