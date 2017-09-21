31ad734e741a21a42c5779b435a7649ac3ed2d17
RazAlgethi <casse.clement@outlook.com>
2017-09-21 23:58:23 +0200

    Enhancement of radial partition graph. - labels set to the side - adding pointers
    ...Ugly code
    ... increasing technical dept.

--- src/pages/resume/Skill/Graph/RadialPartitionGraph.jsx
+++ src/pages/resume/Skill/Graph/RadialPartitionGraph.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import * as d3 from 'd3';
+import * as DataUtils from './DataUtils';
 
 import './radialpartitiongraph.css';
-import * as DataUtils from './DataUtils';
 
 export default class RadialPartitionGraph extends React.Component {
 
@@ -15,7 +15,9 @@ export default class RadialPartitionGraph extends React.Component {
         this.partition = d3.partition()
             .size([2 * Math.PI, Math.min(props.width, props.height) / 2]);
 
-        
+        this.colourScale = d3.scaleLinear()
+            .domain([0, 3])
+            .range([d3.rgb('#E5F0ED'), d3.rgb('#00635A')]);
 
         this.d3logic = this.d3logic.bind(this);
     }
@@ -51,21 +53,64 @@ export default class RadialPartitionGraph extends React.Component {
             .selectAll('g.arc')
             .data(root.descendants());
 
-        arcs.selectAll('text')
-            .text(d => {console.log(d); return d.data.title[lang]});
+        graph.selectAll('text')
+            .text(d => d.data.title[lang]);
 
         const newArcs = arcs.enter();
 
-        newArcs.append('g')
+        newArcs.append('path')
             .classed('arc', true)
-                .append('path')
-                .attr('display', d => (d.depth ? null : 'none'))
-                .attr('d', arc)
-                .style('fill', d => 'white');
+            .attr('display', d => (d.depth ? null : 'none'))
+            .attr('d', arc)
+            .style('fill', d => this.colourScale(d.depth));
 
         newArcs.append('text')
-            .attr('transform', d => `translate(${arc.centroid(d)})`)
+            .style('text-anchor', (d) => {
+                if (d.depth <= 1) {
+                    return 'middle';
+                }
+                if (arc.centroid(d)[0] <= 0) {
+                    return 'end';
+                }
+                return 'start';
+            })
+            .attr('transform', (d) => {
+                const { width } = this.props;
+                const textPosition = arc.centroid(d);
+                if (d.depth <= 1) {
+                    textPosition[0] *= 0.7;
+                    textPosition[1] *= 0.8;
+                    return `translate(${textPosition})`;
+                }
+                if (textPosition[0] <= 0) {
+                    textPosition[0] = -(width / 2) + 50;
+                    textPosition[1] *= 1.3;
+                } else {
+                    textPosition[0] = (width / 2) - 50;
+                    textPosition[1] *= 1.3;
+                }
+                return `translate(${textPosition})`;
+            })
             .text(d => d.data.title[lang]);
+
+        newArcs.append('polyline')
+            .attr('points', (d) => {
+                const { width } = this.props;
+                const arcCenter = arc.centroid(d);
+                const textPosition = arc.centroid(d);
+                if (d.depth <= 1) {
+                    return '';
+                }
+                if (textPosition[0] <= 0) {
+                    textPosition[0] = -(width / 2) + 50 + 4;
+                    textPosition[1] *= 1.3;
+                } else {
+                    textPosition[0] = (width / 2) - 50 - 4;
+                    textPosition[1] *= 1.3;
+                }
+                const bendPoint = [(arcCenter[0] + textPosition[0]) / 2, textPosition[1]];
+                return `${arcCenter} ${bendPoint} ${textPosition}`;
+            });
     }
 
     render() {
