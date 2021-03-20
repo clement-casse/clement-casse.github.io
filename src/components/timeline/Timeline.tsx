93dbf8c16facc4591173d100ed3f624161e11ee6
Clément Cassé <casse.clement@outlook.com>
2021-03-20 16:32:06 +0100

    Introducing timeline in CV to present the project

---
+++ src/components/timeline/Timeline.tsx
@@ -0,0 +1,128 @@
+/* Inspired by https://www.w3schools.com/howto/howto_css_timeline.asp */
+import * as React from 'react'
+import styled from '@emotion/styled'
+import { colors } from '../../styles/variables'
+
+
+export interface TimelineContainerProps {
+  title: string
+  pos?: string
+}
+
+export const Timeline = styled.div`
+  position: relative;
+  max-width: 1200px;
+  margin: 0 auto;
+
+  &::after {
+    content: '';
+    position: absolute;
+    width: 6px;
+    background-color: ${colors.brand};
+    top: 0;
+    bottom: 0;
+    left: 50%;
+    margin-left: -3px;
+  }
+`
+
+const BaseTimelineContainer = styled.div`
+  padding: 10px 40px;
+  position: relative;
+  background-color: inherit;
+  width: 50%;
+
+  &::after {
+    content: '';
+    position: absolute;
+    width: 25px;
+    height: 25px;
+    right: -13px;
+    background-color: white;
+    border: 4px solid ${colors.brand};
+    top: 20px;
+    border-radius: 50%;
+    z-index: 1;
+  }
+`
+
+const RightTimelineContainer = styled(BaseTimelineContainer)`
+  left: 50%;
+
+  &::before {
+    content: " ";
+    height: 0;
+    position: absolute;
+    top: 22px;
+    width: 0;
+    z-index: 1;
+    left: 30px;
+    border: medium solid ${colors.brand};
+    border-width: 10px 10px 10px 0;
+    border-color: transparent ${colors.brand} transparent transparent;
+  }
+
+  &::after {
+    left: -12px;
+  }
+`
+
+const LeftTimelineContainer = styled(BaseTimelineContainer)`
+  left: 0;
+
+  &::before {
+    content: " ";
+    height: 0;
+    position: absolute;
+    top: 22px;
+    width: 0;
+    z-index: 1;
+    right: 30px;
+    border: medium solid ${colors.brand};
+    border-width: 10px 0 10px 10px;
+    border-color: transparent transparent transparent ${colors.brand};
+  }
+`
+
+const TimelineContainerContent = styled.div`
+  padding: 20px 30px;
+  background-color: white;
+  position: relative;
+  border: 1px solid ${colors.brand};
+  border-radius: 6px;
+`
+
+
+const ContentTitle = styled.strong`
+`
+
+
+export const TimelineContainer: React.FC<TimelineContainerProps> = ({title, pos, children}) => {
+  if (pos == "right") {
+    return (
+      <RightTimelineContainer>
+        <TimelineContainerContent>
+          <p>
+            <ContentTitle>{title}</ContentTitle>
+          </p>
+          <p>
+            {children}
+          </p>
+        </TimelineContainerContent>
+      </RightTimelineContainer>
+    )
+  } else {
+    return (
+      <LeftTimelineContainer>
+        <TimelineContainerContent>
+          <p>
+            <ContentTitle>{title}</ContentTitle>
+          </p>
+          <p>
+            {children}
+          </p>
+        </TimelineContainerContent>
+      </LeftTimelineContainer>
+    )
+  }
+}
--- src/pages/cv.tsx
+++ src/pages/cv.tsx
@@ -4,6 +4,7 @@ import styled from '@emotion/styled'
 import Page from '../components/Page'
 import Container from '../components/Container'
 import IndexLayout from '../layouts'
+import { Timeline, TimelineContainer } from '../components/timeline/Timeline'
 import { Link } from 'gatsby'
 
 const StyledButtonBlock = styled.div`
@@ -56,12 +57,62 @@ export default () => (
           </StyledButton>
         </StyledButtonBlock>
 
-        <div>
-          <p>
-            for all decisions related to typography, colors and layout, I referred to{' '}
-            <a href="https://colinecaillier.com/">Coline Caillier</a>.
-          </p>
-        </div>
+        <p>
+          for all decisions related to typography, colors and layout, I referred to{' '}
+          <a href="https://colinecaillier.com/">Coline Caillier</a>.
+        </p>
+
+        <h2>How it is made and why?</h2>
+        <p>
+          I use this page and this website more generally to create my CV.
+          I decided to pick up frameworks, libraries and technologies as I wanted to learn them, as a result, the solution
+        </p>
+
+        <h3>Background</h3>
+        <p>
+          <strong>At the beginning of this website, back in 2015, there was simply a HTML file.</strong>{' '}
+          It was not public, not even served by any web server, just a working document whose goal was to rendered a printer-friendly CV.
+          The initial goal was using raw HTML and CSS to express both the structure and the page setup of this very important document
+          when school is finished, the CV.
+          But, as time flew, this simple side-project has grown up to become a project aiming to showcase some skills I accumulated
+          over the working experiences I had.
+          However I am not a Front-End Developper, I maintain this project and learn new Front-End technologies as a hobbie.
+          My main area of interest remains on Cloud-Computing, Distributed Systems, Monitoring & DevOps.
+        </p>
+        <p>
+
+        </p>
+
+        <h3>The Chronology</h3>
+
+        <Timeline>
+
+          <TimelineContainer title="2015: Creation of an HTML Version of the CV to be printer friendly">
+            An HTML for the structure and the content and a CSS file for page setup, that was all.
+            Creating multiple version of the document to match job offers was way more tedious than I anticipated, especially creating an english version.
+          </TimelineContainer>
+
+          <TimelineContainer title="2016: Adding logic and interaction with AngularJS" pos="right">
+            Making the CV more interactive with Javascript and Angular.js v1 to be served by GitHub&nbsp;Pages.{' '}
+            <a href="https://github.com/clement-casse/clement-casse.github.io/tree/e3d4bec8f0d8b95df56c89af07e47d84e4e764a6">Link to the source code</a>
+          </TimelineContainer>
+
+          <TimelineContainer title="2017: Switch to React / Webpack & D3.js">
+            Due to Angular API changes requirering to relearn the framework, I decided to try another framework : React.
+            Indeed I found the approach of React made on functionnal components clearer to maintain and requiring less knowledge of the framework.
+            It was also a pretext to try a new tool and doing the same coding exercice with different building blocks.{' '}
+            <a href="https://github.com/clement-casse/clement-casse.github.io/tree/f45bf6e04e9c319303e842ed5e919fb75803316d">Link to the source code</a>
+          </TimelineContainer>
+
+          <TimelineContainer title="2021: Integrating React Components into Gatsby static site generator" pos="right">
+            After using Scala for many functional programing during my PhD I decided to give a chance to Typescript in order to retrieve find the confort
+            of static typing for frontend.
+            I also decided to use Gatsby as static site generator, it was an opportuinity to try GraphQL as it is the way Gatsby handles data.
+            It allowed me decoupling components in a clearer way to keep the structure quite simple.
+          </TimelineContainer>
+
+        </Timeline>
+
       </Container>
     </Page>
   </IndexLayout>
